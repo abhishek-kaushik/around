1ecd4bb359659359ae13ddb4bf1d7a4d58fa3bec
Abhishek Kaushik <abhishek@perk.com>
2016-05-28 17:41:28 +0530

    twitter login and migration
    Signed-off-by: Abhishek Kaushik <abhishek@perk.com>

---
+++ app/Http/Controllers/TwitterController.php
@@ -0,0 +1,126 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
+use Thujohn\Twitter\Facades\Twitter;
+
+/**
+ * Class TwitterController
+ * @package App\Http\Controllers
+ */
+class TwitterController extends Controller
+{
+    /**
+     * @var bool
+     */
+    protected $signInTwitter    = true;
+
+    /**
+     * @var bool
+     */
+    protected $forceLogin       = false;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * TwitterController constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request  = $request;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function login()
+    {
+        Twitter::reconfig(['token' => '', 'secret' => '']);
+        $token = Twitter::getRequestToken(route('twitter::callback'));
+
+        if (isset($token['oauth_token_secret'])) {
+            $url = Twitter::getAuthorizeURL($token, $this->signInTwitter, $this->forceLogin);
+
+            Session::put('oauth_state', 'start');
+            Session::put('oauth_request_token', $token['oauth_token']);
+            Session::put('oauth_request_token_secret', $token['oauth_token_secret']);
+
+            return redirect($url);
+        }
+
+        return redirect('twitter::error');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function callback()
+    {
+        if (Session::has('oauth_request_token')) {
+            $requestToken = [
+                'token'  => Session::get('oauth_request_token'),
+                'secret' => Session::get('oauth_request_token_secret'),
+            ];
+
+            Twitter::reconfig($requestToken);
+
+            $oauthVerifier = false;
+
+            if ($this->request->input('oauth_verifier')) {
+                $oauthVerifier = $this->request->input('oauth_verifier');
+            }
+
+            $token = Twitter::getAccessToken($oauthVerifier);
+
+            if (!isset($token['oauth_token_secret'])) {
+                return redirect('twitter/login')
+                    ->with('flash_error', 'We could not log you in on Twitter.');
+            }
+
+            $credentials = Twitter::getCredentials();
+
+            if (is_object($credentials) && !isset($credentials->error)) {
+                Session::put('access_token', $token);
+
+                return redirect('twitter/home')
+                    ->with('flash_notice', 'Congrats! You\'ve successfully signed in!');
+            }
+
+            return redirect('twitter/error')
+                ->with('flash_error', 'Crab! Something went wrong while signing you up!');
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function logout()
+    {
+        Session::forget('access_token');
+        return Redirect::to('twitter/home')
+            ->with('flash_notice', 'You\'ve successfully logged out!');
+    }
+
+    /**
+     * @return string
+     */
+    public function error()
+    {
+        return 'Something went wrong';
+    }
+
+    public function home()
+    {
+        dd($this->request->session());
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::group(
+    [
+        'as'        => 'twitter::',
+        'prefix'    => 'twitter'
+    ],
+    function () {
+        Route::get('home', [
+            'as'    => 'home',
+            'uses'  => 'TwitterController@home'
+        ]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+        Route::get('login', [
+            'as'    => 'login',
+            'uses'  => 'TwitterController@login'
+        ]);
+
+        Route::get('callback', [
+            'as'    => 'callback',
+            'uses'  => 'TwitterController@callback'
+        ]);
+
+        Route::get('logout', [
+            'as'    => 'logout',
+            'uses'  => 'TwitterController@logout'
+        ]);
+    }
+);
